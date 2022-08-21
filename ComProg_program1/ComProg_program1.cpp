4c6183dfb512e3cb4e5f521846c05a8e71fabec6
FighterPaul <65010902@kmitl.ac.th>
2022-08-21 22:15:05 +0700

    Add project files.

---
+++ ComProg_program1/ComProg_program1.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <cstdlib>
+int main()						//Problem 25, Solution 1
+{
+	int n; 
+	scanf("%d", &n);
+	
+	int y = n - 1, x = -(n - 1);
+
+	
+	while (y >= 0)
+	{
+		while (x <= (n - 1))
+		{
+			if (abs(x) + abs(y) <= (n - 1))
+				printf("*");
+			else
+				printf(" ");
+			x = x + 1;
+		}
+		printf("\n");
+		y = y - 1;
+		x = -(n - 1);
+	}
+
+	
+
+	return 0;
+}
